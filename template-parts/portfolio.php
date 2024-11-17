c1990c759f1bd820313cca33eaa827ea0b9ca601
RazelRaz <razel16dcm@gmail.com>
2024-11-17 14:09:29 +0600

    gallery custom post bug fix

--- template-parts/portfolio.php
+++ template-parts/portfolio.php
@@ -26,9 +26,13 @@
 							<?php 
 								$gal_btn_txt = get_post_meta( get_the_ID(), 'gal_key', true );
 								$gal_btn_txt = !empty( $gal_btn_txt ) ? $gal_btn_txt : 'View Details'; // Default text if not set
+
+								// Get the featured image URL
+								$featured_img_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+								$featured_img_url = $featured_img_url ? $featured_img_url : get_template_directory_uri() . '/assets/img/default.jpg'; // Default image fallback
 							?>
 							<div class="single-pf">
-								<img src="<?php echo get_template_directory_uri(); ?>/assets/img/pf1.jpg" alt="#">
+								<img src="<?php echo esc_url($featured_img_url); ?>" alt="<?php echo esc_attr(get_the_title()); ?>">
 								<a href="<?php echo esc_url(get_permalink()); ?>" class="btn"><?php echo esc_html( $gal_btn_txt ); ?></a>
 							</div>
 
